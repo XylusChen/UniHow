626e2beeea2f40a9cfad3ac1b6d5e2061730be00
Xylus Chen Xinyang <chenxinyang00@gmail.com>
2022-07-13 12:39:23 +0000

    browse and search feature added

--- main.py
+++ main.py
@@ -24,9 +24,8 @@ cluster = MongoClient(db_secret)
 db = cluster["telegram"]
 collection = db["unihow"] 
 
-
 # Bot Token
-my_secret = os.environ["API_KEY3"]
+my_secret = os.environ["MYPRECIOUS"]
 bot = telebot.TeleBot(my_secret)
 
 # List of all valid categories for QnA feature 
@@ -57,6 +56,7 @@ def no_back(message):
 
   if len(message.text) > 4:
     return
+    
   bot.send_message(chat_id = message.chat.id, text = "You are already at the main menu. Please use the menu to select what you want to do. " )
 
 
@@ -92,6 +92,7 @@ bot.set_my_commands([
   BotCommand('askquestion', 'Send us your questions!'),
   BotCommand('unanswered', 'View unanswered questions in our QnA Forum!'),
   BotCommand('ansid', "Answer specific Questions using it's question ID!"),
+  BotCommand('browse', 'Browse and search for relevant Questions Sets using QID or Keywords!'),
   BotCommand('livechat', 'Join our Chat Room!'),
   BotCommand('about', 'Find out more about UniHow!'),
   BotCommand('feedback', 'Help us improve! Send us your feedback!'),
@@ -166,7 +167,7 @@ def containsProfanity(message):
 profanity_warning = "Your input contains inappropriate language. We hope to create a safe and positive environment at UniHow that empowers our users to learn more about NUS so as to better shape their university life. Thank you for understanding! Please key in your input again.\n\n*Warning*\nWe seek your cooperation in keeping our UniHow platform a safe and professional one. Inappropriate use of language can be flagged by community members and may result in a permanent ban."
 
 
-#Defining the Ask Question command 
+# Defining the Ask Question command 
 @bot.message_handler(commands=['askquestion'])
 def askQuestion(message):
   username = message.from_user.first_name
@@ -366,7 +367,7 @@ def unanswered_quesV2(message):
 
 
     
-#Answering questions via Category 
+# Answering questions via Category 
 @bot.message_handler(commands=['unanswered'])
 def ansQuestion(message):
   """Answer a Question! """
@@ -611,7 +612,224 @@ def five_posts(dic) :
       return True 
 five_posts_announcement = "Dear users, thank you for using UniHow. We hope that our QnA feature has brought value to you. If you notice any offensive or inappropriate posts, you may report it and the admins will be glad to take a look. It is easy to do so. Simply go to the [UniHow bot](https://t.me/unihow_bot) and send */report*."
 
+# Defining the Browse command
+@bot.message_handler(commands=['browse'])
+def browse(message):
+  """Viewing Portal"""
+  username = message.from_user.first_name
+  first = f"Hi {username}! Welcome to *UniHow QnA Browsing Portal*! \n\n Our browsing portal allows you to conduct targeted searches for specific Question Sets using its unique Question ID or via Category and keywords. To begin, please select your preferred search and filter method. \n\nIf you wish to leave the browsing portal, please send *end*."
+  
+  markup = types.ReplyKeyboardMarkup(row_width = 2, one_time_keyboard = True, resize_keyboard = True)
+  itembtn1 = types.KeyboardButton('Question ID')
+  itembtn2 = types.KeyboardButton('Category & Keywords')
+  markup.add(itembtn1, itembtn2)
+  
+  current = bot.send_message(chat_id = message.chat.id, text = first, reply_markup = markup, parse_mode= 'Markdown')
+  
+  bot.register_next_step_handler(current, filterSearchMethod)
 
+# Processing user's Search Method choice. 
+def filterSearchMethod(message):
+  if userEnd(message) or go_back(message):
+    bot.send_message(chat_id = message.chat.id, text = "Thank you for using our Browsing Portal! Come back anytime to browse for more Question Sets!")
+    return  
+
+  if containsProfanity(message):
+    current = bot.send_message(chat_id = message.chat.id, text = profanity_warning, parse_mode = "Markdown")
+    bot.register_next_step_handler(current, filterSearchMethod)
+    return  
+
+  if message.text == "Question ID":
+    current = bot.send_message(chat_id = message.chat.id, text = "You have selected *Question ID*. \n\n To continue, please send us the Question ID of the question set you wish to view. For example, if I want to view Question Set tagged #7, I will send *7*. Send the number now to view the Question Set. \n\nIf you wish to change your searching method, please send *back*.", parse_mode = "Markdown")
+    bot.register_next_step_handler(current, searchByQID)
+    return
+
+  if message.text == "Category & Keywords":
+    bot.send_message(chat_id = message.chat.id, text = "You have selected *Category & Keywords*.", parse_mode = "Markdown")
+    sendCategoryList(message)
+    messageReply = "To continue, please send us the Category code of the question set you wish to view. \n\nIf you wish to change your searching method, please send *back*."
+    markup = createCatMarkup()
+    current = bot.send_message(chat_id = message.chat.id, text = messageReply, reply_markup = markup, parse_mode = "Markdown")
+    bot.register_next_step_handler(current, searchByCatKey)    
+    return
+
+  # If User Input is invalid. 
+  markup = types.ReplyKeyboardMarkup(row_width = 2, one_time_keyboard = True, resize_keyboard = True)
+  itembtn1 = types.KeyboardButton('Question ID')
+  itembtn2 = types.KeyboardButton('Category & Keywords')
+  markup.add(itembtn1, itembtn2)
+  current = bot.send_message(chat_id = message.chat.id, text = "Your input was invalid. The only two acceptable search methods are by *Question ID* or *Category & Keywords*. Please check your input again. \n\nIf you wish to exit the browsing portal, please type *end*.", parse_mode= 'Markdown', reply_markup = markup)
+  bot.register_next_step_handler(current, filterSearchMethod)
+  return
+
+# Processing user's QID input.
+def searchByQID(message):
+  if go_back(message):
+    markup = types.ReplyKeyboardMarkup(row_width = 2, one_time_keyboard = True, resize_keyboard = True)
+    itembtn1 = types.KeyboardButton('Question ID')
+    itembtn2 = types.KeyboardButton('Category & Keywords')
+    markup.add(itembtn1, itembtn2)
+    current = bot.send_message(chat_id = message.chat.id, text = go_back_message, parse_mode= "Markdown", reply_markup = markup)
+    bot.register_next_step_handler(current, filterSearchMethod)
+    return
+
+  if userEnd(message):
+    bot.send_message(chat_id = message.chat.id, text = "Thank you for using our QnA Browsing Portal! Come back anytime if you want to view more Question Sets!")
+    return
+
+  if message.text == "/ansid":
+    current = bot.send_message(chat_id = message.chat.id, text = "You have been redirected to our answering portal! Please send the Question ID.")
+    bot.register_next_step_handler(current, accept_question_number)
+    return
+  
+  if containsProfanity(message):
+    current = bot.send_message(chat_id = message.chat.id, text = profanity_warning, parse_mode = "Markdown")
+    bot.register_next_step_handler(current, searchByQID)
+    return   
+
+  if not_number(message.text): 
+    current = bot.send_message(chat_id = message.chat.id, text = not_number_text)
+    bot.register_next_step_handler(current, searchByQID)
+    return 
+
+  # Search for Question Sets through database. 
+  # Matching question ID, Unanswered.
+  QID = int(message.text)
+  results_count = collection.count_documents({"_id": QID})
+  
+  # If there are no available questions
+  if results_count == 0:
+      reply = "Oops! There is no active question tagged with this number right now. Please try again with a valid number. If you wish to leave the Browsing Portal, please type *end*."
+      current = bot.send_message(chat_id = message.chat.id, text = reply, parse_mode = 'Markdown')
+      bot.register_next_step_handler(current, searchByQID)
+      return
+  
+  result = collection.find_one({"_id": QID})
+  qns = pickle.loads(result["instance"])
+  # Send Question to user, along with Question ID.
+  category = result["category"]
+  question = result["question"]
+  message1 = "*The question you have selected is:* \n\n" + f"*Category*: {code_to_cat_dict[category]}\n\n" + f"*Question #{QID}*:\n{question}"
+  bot.send_message(chat_id = message.chat.id, text = message1, parse_mode= 'Markdown')
+
+  if qns.get_answercount() == 0:
+    message2 = "This question does not have any responses yet."
+    bot.send_message(chat_id = message.chat.id, text = message2)
+
+  if qns.get_answercount() > 0:
+    message2 = "Here are the responses to this question:"
+    bot.send_message(chat_id = message.chat.id, text = message2)
+    for user in qns.get_answerx5collection().keys():
+      bot.send_message(chat_id = message.chat.id, text = qns.get_answer(user))
+  
+  message3 = f"If you wish to contribute a response to the above question, use our /ansid command. Following which, send {QID} (Question ID). \n\nTo go back and and view a different question set, just send *back*. To leave the browsing portal, send *end*. \n\nDo not that Question Sets that have accumulated more than 5 responses are no longer available for new response contributions." 
+  current = bot.send_message(chat_id = message.chat.id, text = message3, parse_mode= "Markdown")
+  bot.register_next_step_handler(current, searchByQID)
+  return
+      
+user_cat_dic = {}
+
+# Processing user's Category Input
+def searchByCatKey(message):
+  """Check validity of user's Category input"""
+  if go_back(message):
+    markup = types.ReplyKeyboardMarkup(row_width = 2, one_time_keyboard = True, resize_keyboard = True)
+    itembtn1 = types.KeyboardButton('Question ID')
+    itembtn2 = types.KeyboardButton('Category & Keywords')
+    markup.add(itembtn1, itembtn2)
+    current = bot.send_message(chat_id = message.chat.id, text = go_back_message, parse_mode= "Markdown", reply_markup = markup)
+    bot.register_next_step_handler(current, filterSearchMethod)
+    return
+  
+  if userEnd(message):
+    bot.send_message(chat_id = message.chat.id, text = "Thank you for using our QnA Browsing Portal! Come back anytime if you want view more Question Sets!")
+    return
+
+  if containsProfanity(message):
+    current = bot.send_message(chat_id = message.chat.id, text = profanity_warning, parse_mode = "Markdown")
+    bot.register_next_step_handler(current, searchByCatKey)
+    return
+  
+  category = message.text
+  user_cat_dic[message.from_user.id] = category
+  # If User input is Valid. 
+  if validCategory(message):
+    reply = f"You have selected *{code_to_cat_dict[category]}*. \n\nTo continue, please send us the keyword(s) that you wish to further narrow your search by. If you intend to filter your results with more than one keyword, please leave a space between each keyword. \n\n For example, if I wish to input the keyword *coding*, I will send 'coding'. If I wish to input the keywords *workload* and *module*, I will send 'workload module'. \n\nIf you do not wish to input any keywords, please send 'NA'.\n\n To choose a new category, send *back*. To leave the browsing portal, send *end*."
+    current = bot.send_message(chat_id = message.chat.id, text = reply, parse_mode = "Markdown")
+    bot.register_next_step_handler(current, keywordInput)
+    return
+
+  else:
+    reply = "Your input is invalid\. Please respond with a valid Category Code\. Note that the codes are *case sensitive* and you *should not* include the quotation marks\. \n\nIf you wish to leave our Browsing Portal, please type 'end' after this message\."
+    markup = createCatMarkup()
+    current = bot.send_message(chat_id = message.chat.id, text = reply, parse_mode = 'MarkdownV2', reply_markup = markup)
+    bot.register_next_step_handler(current, searchByCatKey)
+    return
+
+# Processing user's keyword(s) input
+def keywordInput(message):
+  if go_back(message):
+    markup = createCatMarkup()
+    current = bot.send_message(chat_id = message.chat.id, text = go_back_message, parse_mode = "Markdown", reply_markup = markup)
+    bot.register_next_step_handler(current, searchByCatKey)
+    return
+
+  if userEnd(message):
+    bot.send_message(chat_id = message.chat.id, text = "Thank you for using our QnA Browsing Portal! Come back anytime if you want to view more Question Sets!")
+    return
+
+  if containsProfanity(message):
+    current = bot.send_message(chat_id = message.chat.id, text = profanity_warning, parse_mode = "Markdown")
+    bot.register_next_step_handler(current, searchByQID)
+    return  
+  
+  category = user_cat_dic[message.from_user.id]
+  results_count = collection.count_documents({"category": category})
+  if results_count == 0:
+    markup = createCatMarkup()
+    current = bot.send_message(chat_id = message.chat.id, text = "Oops! There are currently no Question Sets under your specified Category, please search with a new category.", reply_markup = markup)
+    bot.register_next_step_handler(current, searchByCatKey)
+    return
+
+  results = collection.find({"category": category})
+  
+  if message.text == "NA":
+    for result in results:
+      qID = result["_id"]
+      id_text = f"*#{qID}*"
+      q_string = result["question"]
+      final = f"{q_string}\n\n{id_text}"
+      bot.send_message(chat_id = message.chat.id, text = final, parse_mode = "Markdown")
+    current = bot.send_message(chat_id = message.chat.id, text = "To view the responses to the above question(s), please send the corresponding Question ID")
+    bot.register_next_step_handler(current, searchByQID)
+    return
+
+  keywordList = message.text.split()
+  count = 0
+  for result in results:
+    if containKeyword(keywordList, result):
+      count += 1 
+      qID = result["_id"]
+      id_text = f"*#{qID}*"
+      q_string = result["question"]
+      final = f"{q_string}\n\n{id_text}"
+      bot.send_message(chat_id = message.chat.id, text = final, parse_mode = "Markdown")
+
+  if count == 0:
+    current = bot.send_message(chat_id = message.chat.id, text = "Oops! There are current no Question Sets that contain the keywords you have keyed in, please try again with a different set of keywords!")
+    bot.register_next_step_handler(current, keywordInput)
+
+  else:
+    current = bot.send_message(chat_id = message.chat.id, text = "To view the responses to the above question(s), please send the corresponding Question ID.")
+    bot.register_next_step_handler(current, searchByQID)
+    return    
+
+def containKeyword(keywordList, result):
+  for keyword in keywordList:
+    if keyword not in result["question"]:
+      return False
+  return True
+    
 # Processing User's Feedback Input.
 def acceptFeedback(message):
   """Accepting a user's feedback"""
@@ -641,6 +859,7 @@ def acceptFeedback(message):
   bot.send_message(chat_id = message.chat.id, text = "Thank you for your feedback! Your input has been successfully recorded. We greatly appreciate your efforts in helping us improve. Come back anytime if you have more comments or suggestions for us!")
   bot.send_message(chat_id = -1001541900629, text = feedback, parse_mode = "Markdown")
 
+  
 # Defining the report command.
 @bot.message_handler(commands=['report'])
 def report(message):
@@ -672,7 +891,11 @@ def filterReportCat(message):
     return
 
   # If User Input is invalid. 
-  current = bot.send_message(chat_id = message.chat.id, text = "Your input was invalid. The only two acceptable inputs are *QnA Forum* and *Live Chat*. Please check your input again. \n\nIf you do not wish to file a report anymore, please type *end*.", parse_mode= 'Markdown')
+  markup = types.ReplyKeyboardMarkup(row_width = 2, one_time_keyboard = True, resize_keyboard = True)
+  itembtn1 = types.KeyboardButton('QnA Forum')
+  itembtn2 = types.KeyboardButton('Live Chat')
+  markup.add(itembtn1, itembtn2)
+  current = bot.send_message(chat_id = message.chat.id, text = "Your input was invalid. The only two acceptable inputs are *QnA Forum* and *Live Chat*. Please check your input again. \n\nIf you do not wish to file a report anymore, please type *end*.", parse_mode= 'Markdown', reply_markup = markup)
   bot.register_next_step_handler(current, filterReportCat)
   return
 
@@ -747,7 +970,7 @@ def resettimer(message):
 def start(message):
   """Welcome new User!"""
   username = message.from_user.first_name
-  messageReply = f"Hi {username}! Welcome to UniHow! \n\n/gipanel to learn more about programs and accomodation options within NUS ! \n\n/askquestion if you want to ask a question! \n\n/unanswered to check out available questions to answer! \n\n/ansid to answer a question using its unique Question ID! \n\n/view to browse and search for Question Sets! \n\n/livechat to engage in a real-time conversation with a University senior or Professor!"
+  messageReply = f"Hi {username}! Welcome to UniHow! \n\n/gipanel to learn more about programs and accomodation options within NUS ! \n\n/askquestion if you want to ask a question! \n\n/unanswered to check out available questions to answer! \n\n/ansid to answer a question using its unique Question ID! \n\n/browse to browse and search for Question Sets! \n\n/livechat to engage in a real-time conversation with a University senior or Professor!"
   bot.reply_to(message, messageReply)
 
   bot.send_message(chat_id = message.chat.id, text = "We urge all users to behave responsibly on UniHow, especially when using our QnA Forum and LiveChat features. It takes a communal effort to keep this platform *safe* and *professional* so that everyone can use it with a peace of mind. If you notice any signs of misconduct or inappropriacy, please let us know by using our /report function, more detailed instructions will follow.", parse_mode = "Markdown")
